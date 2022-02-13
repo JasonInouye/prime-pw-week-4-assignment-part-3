5cc496f23081850814590fd4afe80dc98821d69c
Jason Inouye <jasonjinouye@gmail.com>
2022-02-13 09:52:34 -0600

    Added empty function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -7,18 +7,42 @@ let basket = [];
 
 console.log( `The array basket`, basket);
 
-function addToBasket( addItem ){
-  console.log( `inside addItem`);
-  basket.push( addItem );
+// Create a function called addItem
+function addItem( addtobasket ){
+  console.log( `inside addtobasket`);
+  basket.push( addtobasket );
   return true;
 } //end addToBasket
 
-addToBasket( `Socks` );
-addToBasket( `Shoes` );
-addToBasket( `Football` );
-addToBasket( `Jersey` );
+// Add items to array testing
+addItem( `Socks` );
+addItem( `Shoes` );
+addItem( `Football` );
+addItem( `Jersey` );
 
-console.log( `log test addToBasket Socks`, addToBasket( `Socks` ) );
-console.log( `log test addToBasket Shoes`, addToBasket( `Shoes` ) );
-console.log( `log test addToBasket Football`, addToBasket( `Football` ) );
-console.log( `log test addToBasket Jersey`, addToBasket( `Jersey` ) );
+console.log( `log function test Helmet`, addItem( `Helmet` ) );
+
+
+// Create a function called listItems
+
+function listItems() {
+  for ( let eachItem of basket ) {
+    console.log( `Item's in the basket are ${eachItem}` );
+  } // End of for of loop
+} // End of listItems
+
+// listItems();
+
+console.log( `log test listItemss`, listItems() );
+
+// Create a function called empty
+listItems();
+
+function empty() {
+  while (basket.length > 0) {
+    console.log( `Items remaining in basket`, basket );
+    basket.pop(basket);
+  }
+}
+
+empty();
