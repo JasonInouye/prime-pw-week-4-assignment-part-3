33ab86a66801893402ec7a81f01b86e0d1aa5d93
Jason Inouye <jasonjinouye@gmail.com>
2022-02-13 00:17:36 -0600

    added basket array

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,8 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// Create a global variable named basket
+let basket = [];
+
+console.log( `The array basket`, basket);
